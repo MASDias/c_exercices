9857b9194ceb48d86a39aac6b34b708c1ab359fe
Mario <1151708@isep.ipp.pt>
2017-10-11 14:31:21 +0100

    Correcao ex 15

--- modulo1/ex15/consecutiveSets.c
+++ modulo1/ex15/consecutiveSets.c
@@ -2,32 +2,30 @@
 #include <stdlib.h> // conflitos na compilação com o rand() tive que importar esta biblioteca 
 #include <time.h>
 
-
-int verificationSet(int* vec){
-	if (*(vec-1) < *vec && *(vec+1) > *vec ){
-		return 1;
-	}
-	return 0;
-}
-
-void incremental(int* vec, int tSet){
-	if (verificationSet(vec) == 1){
-		tSet+=1;
-		printf("INC %d\n",tSet);
-	}
-}
-
 void populateVector(int* vec, int n){
 	int i;
-
 	srand(time(NULL)); // usa como referencia o tempo para poder gerar novos numeros aleatorios 
 	for (i = 0; i < n; i++){
 		do{
 			*(vec+i) = rand()%100;
 		} while (*(vec+i) > 20);
-	}
-	
+	}	
 }
+
+int verificationSet(int* vec){
+	if (*(vec-1) < *(vec) && *(vec+1) > *vec ){
+		return 1;
+	}
+	return 0;
+}
+
+void incremental(int* vec, int* tSet){
+	int check = verificationSet(vec);
+	if (check == 1){
+		*tSet+=1;
+	}
+}
+
 
 void consecutiveSets(int* vec,int n){
 	populateVector(vec,n);
@@ -36,5 +34,4 @@ void consecutiveSets(int* vec,int n){
 	for (i = 1; i < n-1; i++){
 		incremental(vec+i,&inc);
 	}
-	printf("MAIN %d\n",inc);
 }
--- modulo1/ex15/main.c
+++ modulo1/ex15/main.c
@@ -2,7 +2,7 @@
 #define MAX_NUMBER 100
 
 int main(void){
-	int vec[MAX_NUMBER];
+	int vec[MAX_NUMBER] = {};
 	consecutiveSets(vec,MAX_NUMBER);
 	return 0;
 }
