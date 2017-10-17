5a4b5b5d67ebfa226836abfee72d3e93115446d9
Mario <1151708@isep.ipp.pt>
2017-10-17 11:28:52 +0100

    Modulo 2 - Exercicio 7 para resolver

--- modulo2/ex05/swap.s
+++ modulo2/ex05/swap.s
@@ -6,10 +6,10 @@ swap:
 #prologue
 	pushl %ebp # save previous stack frame pointer
 	movl %esp, %ebp # the stack frame pointer for sum function
-	mov swapper, %ax # gives the value of swapper to %ax (Instrução sem "l" para instruções de menos de 32 bit)
-	mov %ah, %bl #os bits mais significativos a %bl
-	mov %al, %bh #os bits menos significativos para %bh
-	mov %bx, %ax #agora que foi trocado os bits, declara %bx em %ax, dando um novo  valor com a inversão de bits
+	movw swapper, %ax # gives the value of swapper to %ax (Instrução sem "l" para instruções de menos de 32 bit)
+	movb %ah, %bl #os bits mais significativos a %bl
+	movb %al, %bh #os bits menos significativos para %bh
+	movw %bx, %ax #agora que foi trocado os bits, declara %bx em %ax, dando um novo  valor com a inversão de bits
 # epilogue
 	movl %ebp, %esp # restore the previous stack pointer ("clear" the stack)
 	popl %ebp # restore the previous stack frame pointer
