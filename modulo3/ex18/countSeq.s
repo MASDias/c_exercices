81784954b429499359d67075bb54b99713b04589
Mario <1151708@isep.ipp.pt>
2017-11-05 15:42:50 +0000

    Correcao exercicio 18

--- modulo3/ex18/countSeq.s
+++ modulo3/ex18/countSeq.s
@@ -16,18 +16,18 @@ countSeq:
 	movl $0, %ecx
 	movl $0, %ebx
 	pushl num
-	subl $3, num
-	movl $vecptr, %esi		#declara o apontador de ptrvec a esi 
+	subl $2, num
+	movl $vecptr, %esi				#declara o apontador de ptrvec a esi 
 seq_loop:
-	cmpl num, %ebx			#verifica se vetor acabou
-	je end					#acaba o ciclo caso seja igual ao num
+	cmpl num, %ebx					#verifica se vetor acabou
+	je end							#acaba o ciclo caso seja igual ao num
 	
-	pushl %ebx				#guarda a posição do ciclo 
+	pushl %ebx						#guarda a posição do ciclo 
 	
-	movl (%esi), %edx		#guarda termo de comparação
-	call seq1				#chama funcao seq 1 , que verifica 'v(i) < v(i+1)'
+	movl (%esi,%ebx,4), %edx		#guarda termo de comparação
+	call seq1						#chama funcao seq 1 , que verifica 'v(i) < v(i+1)'
 	cmp $1, %eax
-	jne next_it				#se é diferente de 1, entao passa a proxima iteração
+	jne next_it						#se é diferente de 1, entao passa a proxima iteração
 
 	movl (%esi,%ebx,4), %edx
 	call seq2
@@ -36,13 +36,11 @@ seq_loop:
 	
 next_it:					#proxima iteração
 	popl %ebx
-	addl $4, %esi			#passa à proxima posicao do vetor
 	incl %ebx				#incrementa no loop
 jmp seq_loop				#volta ao inicio do loop
 inc:
 	incl %ecx
 	popl %ebx
-	addl $4, %esi			#passa à proxima posicao do vetor
 	incl %ebx				#incrementa no loop
 	jmp seq_loop
 end:
