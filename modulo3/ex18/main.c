ba516f44fa0da92ffe63b5dca68baeb927d7ab52
Mario <1151708@isep.ipp.pt>
2017-11-05 14:49:47 +0000

    Modulo 3 3 - Exercicio 18 resolvido

--- modulo3/ex18/main.c
+++ modulo3/ex18/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "countSeq.h"
 int num=8;
-int vecptr[] = {1,2,4,3,3,5,6,9};
+int vecptr[] = {1,5,4,3,3,5,6,9};
 int main(void){
 	int i=0;
 	int total = countSeq();
--- modulo3/ex18/seq1.s
+++ modulo3/ex18/seq1.s
@@ -10,7 +10,7 @@ seq1:
 	movl $0, %eax
 	incl %ebx
 	cmpl %edx, (%esi,%ebx,4)
-	jl end
+	jg end
 	movl $1, %eax
 end:
 #epilogue
