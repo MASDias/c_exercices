90c73844ae79f63bd971ae3202d0aed4b8aa0361
Miguel <1161386@isep.ipp.pt>
2017-11-28 17:54:22 +0000

    Modulo 5 - Exercicio 1 resolvido

---
+++ modulo5/ex01/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "save_data.h"
+#include "student.h"
+	
+int main(void){
+	Student p;
+	Student* ptr_p = &p;
+	int age = 12;
+	int number = 10;
+	char name[80] = "Miguel Dias";
+	char address[120] = "Rua 9 de julho";
+	
+	save_data(ptr_p, age, number, name, address);
+	
+	printf("\nAge: %d\n", ptr_p->age);
+	printf("Number: %d\n", ptr_p->number);
+	printf("Name: %s\n", ptr_p->name);
+	printf("Address: %s\n\n", ptr_p->address);
+	
+	
+	return 0;
+}
---
+++ modulo5/ex01/save_data.h
@@ -0,0 +1,5 @@
+#include "student.h"
+#ifndef SAVEDATA_H
+#define SAVEDATA_H
+	void save_data(Student*, int, int, char*, char*);
+#endif
