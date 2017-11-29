b30e62c491deb8f811e972b6e13de45ac7ad053a
Mario <1151708@isep.ipp.pt>
2017-11-29 15:41:32 +0000

    ExeModulo 5 - Exercicio 4 correcao

--- modulo5/ex04/save_grades.s
+++ modulo5/ex04/save_grades.s
@@ -10,16 +10,15 @@ save_grades:
 	pushl %esi
 	pushl %edi
 #body
-	movl $0, %ecx
-	movl 8(%ebp), %esi
-	movl 12(%ebp), %edi
-	
+	movl $0, %ecx					#inicializar o indice para o ciclo
+	movl 8(%ebp), %esi				#endereçar esi com a struct de estudante
+	movl 12(%ebp), %edi				#endereçar edi com o vetor de grades
 grades_loop:
-	cmpl $10,%ecx
-	je end
-	movl (%edi,%ecx,4), %edx
-	movl %edx, 8(%esi,%ecx,4)
-	incl %ecx
+	cmpl $10,%ecx					#verificação de fim do ciclo
+	je end							
+	movl (%edi,%ecx,4), %edx		#dar ao registo %edx o valor correspondente a posicao %ecx 
+	movl %edx, 8(%esi,%ecx,4)		#mover a nota que esta no registo %edx para a struct que esta no registo %esi, com o offset de 8 na podicao %ecx
+	incl %ecx						#incrementar contador
 jmp grades_loop
 end:
 #epilogue
